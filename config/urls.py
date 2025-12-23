d5a1d3fd67998be9aaa629124130bdfa882d8d60
“Yusuke0324-create” <hamupupp0324@gmail.com>
2025-12-24 05:43:22 +0900

    もう一回サイトマップ

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,36 @@
-# config/urls.py
-
 from django.contrib import admin
-from django.urls import path, include, re_path # re_path をインポート
+from django.urls import path, include, re_path, reverse
 from django.views.generic import RedirectView
 from django.conf import settings
+from django.views.static import serve
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
 
-from django.conf.urls.static import static 
-# ↓ サーブビューをインポート
-from django.views.static import serve 
+# サイトマップ設定
+class StaticViewSitemap(Sitemap):
+    priority = 1.0
+    changefreq = 'daily'
 
+    def items(self):
+        return ['campus_guide:university_list', 'campus_guide:search']
+
+    def location(self, item):
+        return reverse(item)
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
+# URL
 urlpatterns = [
     path('campus_guide/', include('campus_guide.urls')),
     path('sibuyasky1219/', admin.site.urls),
     path('', RedirectView.as_view(pattern_name='campus_guide:university_list')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 
-    # メディアファイルを本番環境で配信するための設定
+    # サイトマップ
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
+    # メディアファイル配信
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
 ]
-
-# 以前の urlpatterns += static(...) の行は不要なので削除
